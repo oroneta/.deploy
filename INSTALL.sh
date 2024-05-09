f69c08844325d78013f5d756e6753b5d7de5e8d5
Zheng Lin Lei <zheng9112003@gmail.com>
2024-05-10 01:21:09 +0200

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,11 @@
+ls
+
+git clone https://github.com/oroneta/drone-front.git
+git clone https://github.com/oroneta/drone-module.git
+git clone https://github.com/oroneta/core-system.git
+
+
+chmod +x RUN.sh
+
+
+./RUN.sh
